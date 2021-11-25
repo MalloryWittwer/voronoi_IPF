480c40da721973686c9eef542b23d42c018030c8
MalloryWittwer <mallory.wittwer@gmail.com>
2021-11-25 22:07:33 +0100

    Clean initial commit

---
+++ app.py
@@ -0,0 +1,128 @@
+import numpy as np
+import dash
+from dash import html, State, dcc
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+from dash.dependencies import Input, Output
+from serve_voronoi import get_voronoi, fig_to_uri, fill_plot_outline, set_colorbar, get_region_values, compute_vectors
+import matplotlib.pyplot as plt
+from scipy.spatial import voronoi_plot_2d
+
+app = dash.Dash(__name__, title='IPF Plot Experiment', 
+                external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.CYBORG])
+
+server = app.server
+
+EULERS = np.load('eulers_demo.npy')
+VALUES = np.load('values_demo.npy')
+
+def serve_layout():
+    layout = html.Div([
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.Img(src="assets/hexagons-white.png", id="tiles-label"),
+                    html.Div([
+                        dcc.Slider(id="input-tiles", min=100, max=800, step=100, value=400, tooltip={"placement": "bottom", "always_visible": True}),
+                    ], id="tiles-container"),
+                    html.Img(src="assets/hexagons-white-many.png", id="tiles-label-many"),
+                ], id="grid-container-r1"),
+                html.Div([  
+                    html.Img(src="assets/3d-cube-white.png", id="radios-label"),
+                    dcc.RadioItems(
+                        options=[
+                            {'label': 'X', 'value': 'x'},
+                            {'label': 'Y', 'value': 'y'},
+                            {'label': 'Z', 'value': 'z'}
+                        ], value='z', id="radio-items"),
+                ], id="grid-container-r2"),
+                html.Div([
+                    html.Div([
+                        html.Label("Min", htmlFor="min-input", id="min-label"),
+                        dcc.Input(type="number", value=0, id="min-input")
+                    ], id="min-container"),
+                    html.Div([
+                        html.Label("Max", htmlFor="max-input", id="max-label"),
+                        dcc.Input(type="number", value=20, id="max-input")
+                    ], id="max-container"),
+                ], id="grid-min-max"),
+                html.Div([
+                    html.Div([
+                        html.Img(src="assets/icon-lw.png", id="lw-label"),
+                        daq.BooleanSwitch(id='checklist', on=True),
+                    ], id='line-width-container'),
+                    dcc.Dropdown(
+                        id='colormap-dropdown',
+                        options=[
+                            {'label': 'Blues', 'value': 'Blues'},
+                            {'label': 'Grays', 'value': 'gray'},
+                            {'label': 'Viridis', 'value': 'viridis'},
+                            {'label': 'Inferno', 'value': 'inferno'},
+                        ], value='Blues',
+                    ),
+                ], id="grid-colormap"),
+            ], id="pannel"),
+            html.Div([
+                dcc.Loading(id="loading", type="default", children=html.Img(src="assets/default_img.png", id="plt-figure"))
+            ], id="canvas")
+        ], id="content-container"),
+        html.Div([
+            html.A("About", id="about", n_clicks=0),
+            html.A("View code", id="code", href="#", target="_blank"),
+            dbc.Modal([
+                dbc.ModalBody("In a face-centered cubic material, the orientation distribution of a generic property of interest (ex: misorientation) of the crystallites is displayed. The value of the property is averaged locally in the orientation space, within discrete bins defined by the stereographic projection of a Fibonacci sphere and confined in the inverse pole figure triangle."),
+                html.Img(src="assets/figure-about.png", id="modal-image"),
+                dbc.ModalFooter(
+                    dbc.Button("Close", id="close", className="mr-auto", n_clicks=0), 
+                    id="modal-footer"),
+            ], id="modal", is_open=False, backdrop=True, centered=True, fade=True),
+        ], id="footer-container"),
+    ], id="content-wrapper")
+    return layout
+
+app.layout = serve_layout
+
+@app.callback(
+    Output("plt-figure", "src"),
+    Input("checklist", "on"),
+    Input("input-tiles", "value"),
+    Input("radio-items", "value"),
+    Input('min-input', 'value'),
+    Input('max-input', 'value'),
+    Input('colormap-dropdown', 'value'),
+)
+def update_output(chx, nft, dir, vmin, vmax, cm):
+        
+    fig, ax = plt.subplots(figsize=(7,5), dpi=200)
+    fig.patch.set_alpha(0)
+    
+    vectors = compute_vectors(EULERS, dir) # (x, y) coordinates of eulers
+    
+    # ax.scatter(x=vectors[:,0], y=vectors[:,1], c=VALUES, cmap=plt.cm.Blues)
+    
+    lw = 0.5 if chx else 0.0        
+    vor = get_voronoi(nft)    
+    voronoi_plot_2d(vor, show_points=False, show_vertices=False, line_colors='black', 
+                    ax=ax, line_width=lw)
+    region_values = get_region_values(vor, vectors, VALUES)
+    ax = set_colorbar(region_values, ax, vor, vmin, vmax, cm)
+
+    ax = fill_plot_outline(ax)
+    ax.set_ylim(0, 0.40)
+    ax.set_xlim(0, 0.45)
+    ax.axis('off')
+    
+    return fig_to_uri(fig)
+
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("about", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+if __name__ == '__main__':
+    app.run_server(debug=True, dev_tools_hot_reload=False)
---
+++ serve_voronoi.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as clrs
+from sklearn.metrics import pairwise_distances
+from sklearn.preprocessing import normalize
+from scipy.spatial import Voronoi
+from io import BytesIO
+import base64
+
+def fig_to_uri(fig, close_all=True):
+    out_img = BytesIO()
+    fig.savefig(out_img, format='png')
+    if close_all:
+        fig.clf()
+        plt.close('all')
+    out_img.seek(0)  # rewind file
+    encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
+    return "data:image/png;base64,{}".format(encoded)
+
+def compute_vectors(eulers, direction):
+
+    def vector_project(vect):
+        rZ = np.empty(vect.shape[0])
+        theZ = np.empty(vect.shape[0])
+        for k, vect in enumerate(vect):
+            theZ[k] = np.arctan2(vect[1], vect[0])
+            rZ[k]   = np.sin(np.arccos(vect[2]))/(1+np.arccos(np.cos(vect[2])))
+        xes_preds = rZ*np.cos(theZ)
+        yes_preds = rZ*np.sin(theZ)
+        return xes_preds, yes_preds
+
+    i1, i2, i3 = eulers.T
+    i1c = np.cos(i1)
+    i1s = np.sin(i1)
+    i2c = np.cos(i2)
+    i2s = np.sin(i2)
+    i3c = np.cos(i3)
+    i3s = np.sin(i3)
+    x00 = i1c*i2c*i3c-i1s*i3s
+    x01 = -i3c*i1s-i1c*i2c*i3s
+    x02 = i1c*i2s
+    x10 = i1c*i3s+i2c*i3c*i1s
+    x11 = i1c*i3c-i2c*i1s*i3s
+    x12 = i1s*i2s
+    x20 = -i3c*i2s
+    x21 = i2s*i3s
+    x22 = i2c
+    c0 = np.stack((x00,x01,x02), axis=1)
+    c1 = np.stack((x10,x11,x12), axis=1)
+    c2 = np.stack((x20,x21,x22), axis=1)
+    rot_mat = np.stack((c0,c1,c2), axis=1)
+    rot_mat_inv = np.linalg.inv(rot_mat)
+    
+    xv, yv, zv = list(map(lambda idx: np.sort(normalize(np.abs(idx), axis=1), axis=1), 
+                          np.transpose(rot_mat_inv, [2, 0, 1])))
+    
+    if direction=='z':
+        yes, xes = vector_project(zv)
+    elif direction=='y':
+        yes, xes = vector_project(yv)
+    elif direction=='x':
+        yes, xes = vector_project(xv)
+    else:
+        print(f'Wrong direction: {direction}')
+        return 0
+    
+    x_points = np.vstack((xes, yes)).T
+    
+    return x_points
+
+def fill_plot_outline(ax):
+    xcr = np.sqrt(2) * np.cos(np.radians(np.linspace(0, 45, 50))) - 1.0
+    ycr = np.sqrt(2) * np.sin(np.radians(np.linspace(0, 45, 50)))
+    xcr = np.append(xcr, [0.45, 0.45])
+    ycr = np.append(ycr, [0.4,0.0])
+    ax.fill([0.0,0.0,0.4], [0.0,0.4,0.4], 'black', zorder=10)
+    ax.fill(xcr, ycr, 'black', zorder=10)
+    
+    # Add annotations
+    ax.text(-0.02, -0.02, '100', color='white')
+    ax.text(0.40, -0.02, '110', color='white')
+    ax.text(0.354, 0.375, '111', color='white', zorder=11)
+    return ax
+
+def get_region_values(vor, x_points, z_values):
+    knn_idx = np.argmin(pairwise_distances(X=x_points, Y=vor.points, metric='euclidean'), axis=1)
+    region_values = np.zeros((len(vor.points)))
+    n_vals_cell = np.zeros((len(vor.points)))
+    for ind, zval in zip(knn_idx, z_values):
+        n_vals_cell[ind] += 1
+        region_values[ind] += (zval - region_values[ind]) / n_vals_cell[ind]
+    return region_values
+
+def set_colorbar(region_values, ax, vor, vmin, vmax, cmap):
+    mapper = plt.cm.ScalarMappable(norm=clrs.Normalize(vmin=vmin, vmax=vmax, clip=True), cmap=cmap)
+    for r in range(len(vor.point_region)):
+        region = vor.regions[vor.point_region[r]]
+        if not -1 in region:
+            plt.fill(*zip(*[vor.vertices[i] for i in region]), color=mapper.to_rgba(region_values[r]))
+    cb = plt.colorbar(mapper)
+    cb.ax.tick_params(colors='white')
+    return ax
+
+def get_voronoi(n_fib_tiles):
+    points = []
+    phi = np.pi * (3. - np.sqrt(5.))
+    for i in range(n_fib_tiles*16):
+        theta = phi * i        
+        y = 1 - (i / float(n_fib_tiles*16 - 1)) * 2 
+        x = np.cos(theta) * np.sqrt(1 - y * y)
+        z = np.sin(theta) * np.sqrt(1 - y * y) 
+        r = np.sqrt(x**2 + y**2 + z**2)
+        sss = np.array([-x / (1-z), -y / (1-z)])
+        if (np.linalg.norm(sss) <= 0.65) & (np.arccos(z / r) > np.radians(90)) & (x <= 0.1) & (y <= 0.1):
+            points.append(sss)
+    points = np.array(points)
+    vor = Voronoi(points)
+    return vor
